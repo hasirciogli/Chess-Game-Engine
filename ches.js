e4c999a34620ded2cd86050117d476ca0c074442
Mustafa Hasırcıoğlu <86384408+hasirciogli@users.noreply.github.com>
2023-01-27 03:43:31 +0300

    update

--- ches.js
+++ ches.js
@@ -9,63 +9,64 @@ var max = 700;
 
 
 
-var taslar = [
-    {
-        id: 0,
-        name: "Kale",
-        position: { x: 0, y: 0 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 1,
-        name: "At",
-        position: { x: 1, y: 0 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 2,
-        name: "Fil",
-        position: { x: 2, y: 0 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 3,
-        name: "Vezir",
-        position: { x: 3, y: 0 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 4,
-        name: "Şah",
-        position: { x: 4, y: 0 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 5,
-        name: "Fil",
-        position: { x: 5, y: 0 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 6,
-        name: "At",
-        position: { x: 6, y: 0 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 7,
-        name: "Kale",
-        position: { x: 7, y: 0 },
-        isAlive: true,
-        team: 0,
-    },
+var taslar =
+    [
+        {
+            id: 0,
+            name: "Kale",
+            position: { x: 0, y: 0 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 1,
+            name: "At",
+            position: { x: 1, y: 0 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 2,
+            name: "Fil",
+            position: { x: 2, y: 0 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 3,
+            name: "Vezir",
+            position: { x: 3, y: 0 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 4,
+            name: "Şah",
+            position: { x: 4, y: 0 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 5,
+            name: "Fil",
+            position: { x: 5, y: 0 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 6,
+            name: "At",
+            position: { x: 6, y: 0 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 7,
+            name: "Kale",
+            position: { x: 7, y: 0 },
+            isAlive: true,
+            team: 0,
+        },
 
 
 
@@ -74,72 +75,62 @@ var taslar = [
 
 
 
-    {
-        id: 8,
-        name: "Piyon",
-        position: { x: 0, y: 1 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 9,
-        name: "Piyon",
-        position: { x: 1, y: 1 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 10,
-        name: "Piyon",
-        position: { x: 2, y: 1 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 11,
-        name: "Piyon",
-        position: { x: 3, y: 1 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 12,
-        name: "Piyon",
-        position: { x: 4, y: 1 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 13,
-        name: "Piyon",
-        position: { x: 5, y: 1 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 14,
-        name: "Piyon",
-        position: { x: 6, y: 1 },
-        isAlive: true,
-        team: 0,
-    },
-    {
-        id: 15,
-        name: "Piyon",
-        position: { x: 7, y: 1 },
-        isAlive: true,
-        team: 0,
-    },
-
-
-
-
-
-
-
-
-
-
+        {
+            id: 8,
+            name: "Piyon",
+            position: { x: 0, y: 1 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 9,
+            name: "Piyon",
+            position: { x: 1, y: 1 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 10,
+            name: "Piyon",
+            position: { x: 2, y: 1 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 11,
+            name: "Piyon",
+            position: { x: 3, y: 1 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 12,
+            name: "Piyon",
+            position: { x: 4, y: 1 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 13,
+            name: "Piyon",
+            position: { x: 5, y: 1 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 14,
+            name: "Piyon",
+            position: { x: 6, y: 1 },
+            isAlive: true,
+            team: 0,
+        },
+        {
+            id: 15,
+            name: "Piyon",
+            position: { x: 7, y: 1 },
+            isAlive: true,
+            team: 0,
+        },
 
 
 
@@ -155,62 +146,6 @@ var taslar = [
 
 
 
-    {
-        id: 16,
-        name: "Kale",
-        position: { x: 0, y: 7 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 17,
-        name: "At",
-        position: { x: 1, y: 7 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 18,
-        name: "Fil",
-        position: { x: 2, y: 7 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 19,
-        name: "Vezir",
-        position: { x: 3, y: 7 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 20,
-        name: "Şah",
-        position: { x: 4, y: 7 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 22,
-        name: "Fil",
-        position: { x: 5, y: 7 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 23,
-        name: "At",
-        position: { x: 6, y: 7 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 24,
-        name: "Kale",
-        position: { x: 7, y: 7 },
-        isAlive: true,
-        team: 1,
-    },
 
 
 
@@ -219,96 +154,226 @@ var taslar = [
 
 
 
-    {
-        id: 25,
-        name: "Piyon",
-        position: { x: 0, y: 6 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 26,
-        name: "Piyon",
-        position: { x: 1, y: 6 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 27,
-        name: "Piyon",
-        position: { x: 2, y: 6 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 28,
-        name: "Piyon",
-        position: { x: 3, y: 6 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 29,
-        name: "Piyon",
-        position: { x: 4, y: 6 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 30,
-        name: "Piyon",
-        position: { x: 5, y: 6 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 31,
-        name: "Piyon",
-        position: { x: 6, y: 6 },
-        isAlive: true,
-        team: 1,
-    },
-    {
-        id: 32,
-        name: "Piyon",
-        position: { x: 7, y: 6 },
-        isAlive: true,
-        team: 1,
-    },
-];
 
 
-function getTas(x, y, callBack) {
-    taslar.forEach(element => {
-        if (element.position.x == x && element.position.y == y) {
-            callBack(true, element);
-        }
-    });
-    callBack(false, false);
-}
+        {
+            id: 16,
+            name: "Kale",
+            position: { x: 0, y: 7 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 17,
+            name: "At",
+            position: { x: 1, y: 7 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 18,
+            name: "Fil",
+            position: { x: 2, y: 7 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 19,
+            name: "Vezir",
+            position: { x: 3, y: 7 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 20,
+            name: "Şah",
+            position: { x: 4, y: 7 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 22,
+            name: "Fil",
+            position: { x: 5, y: 7 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 23,
+            name: "At",
+            position: { x: 6, y: 7 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 24,
+            name: "Kale",
+            position: { x: 7, y: 7 },
+            isAlive: true,
+            team: 1,
+        },
 
 
 
-for (let i = 0; i < 8; i++) {
 
-    var col = i % 2 == 0 ? false : true;
 
-    for (let i2 = 0; i2 < 8; i2++) {
-        col ? ctx.fillStyle = "rgb(170, 170, 170)" : ctx.fillStyle = "rgb(100, 100, 100)";
 
-        ctx.fillRect((max / 8) * i2, (max / 8) * i, max / 8, max / 8);
 
-        getTas(i2, i, (status, val) => {
-            if (status) {
-                ctx.font = "20px serif";
-                ctx.fillStyle = val.team == 0 ? "black" : "red";
-                ctx.fillText(val.name, ((max / 8) * i2) + ((max / 8 / 2) - (ctx.measureText(val.name).width / 2)), (((max / 8) * i) + 7) + ((max / 8) / 2));
+
+        {
+            id: 25,
+            name: "Piyon",
+            position: { x: 0, y: 6 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 26,
+            name: "Piyon",
+            position: { x: 1, y: 6 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 27,
+            name: "Piyon",
+            position: { x: 2, y: 6 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 28,
+            name: "Piyon",
+            position: { x: 3, y: 6 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 29,
+            name: "Piyon",
+            position: { x: 4, y: 6 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 30,
+            name: "Piyon",
+            position: { x: 5, y: 6 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 31,
+            name: "Piyon",
+            position: { x: 6, y: 6 },
+            isAlive: true,
+            team: 1,
+        },
+        {
+            id: 32,
+            name: "Piyon",
+            position: { x: 7, y: 6 },
+            isAlive: true,
+            team: 1,
+        },
+    ];
+
+
+
+
+class ChessGame {
+    constructor(tData) {
+        this.tData = tData;
+    }
+
+    getTas = async (x, y, callBack) => {
+        var tb = false;
+        await this.tData.forEach(element => {
+            if (element.position.x == x && element.position.y == y) {
+                callBack(true, element);
+                tb = true;
             }
         });
-        
 
-        col = !col;
+        if (!tb)
+            callBack(false, false);
     }
+
+    run = () => {
+        for (let i = 0; i < 8; i++) {
+
+            var col = i % 2 == 0 ? false : true;
+
+            for (let i2 = 0; i2 < 8; i2++) {
+                col ? ctx.fillStyle = "rgb(170, 170, 170)" : ctx.fillStyle = "rgb(100, 100, 100)";
+
+                ctx.fillRect((max / 8) * i2, (max / 8) * i, max / 8, max / 8);
+
+                this.getTas(i2, i, (status, val) => {
+                    if (status && val.isAlive) {
+                        ctx.font = "20px serif";
+                        ctx.fillStyle = val.team == 0 ? "black" : "red";
+                        ctx.fillText(val.name, ((max / 8) * i2) + ((max / 8 / 2) - (ctx.measureText(val.name).width / 2)), (((max / 8) * i) + 7) + ((max / 8) / 2));
+                    }
+                });
+
+
+                col = !col;
+            }
+        }
+    }
+
+
+    makeMove = async (id, x, y) => {
+        var rVal = "";
+        await this.tData.forEach(element => {
+            if (element.id == id) {
+                if (element.position.x == x && element.position.y == y) {
+                    rVal = "????? are you damn ?????";
+                }
+                else {
+                    this.getTas(x, y, (status, val) => {
+                        if (!status) {
+                            rVal = "work";
+                            element.position.x = x;
+                            element.position.y = y;
+                        }
+                        else {
+                            if (val.team != element.team) {
+                                rVal = "work";
+                                if (val.isAlive) {
+                                    val.isAlive = false;
+                                    element.position.x = x;
+                                    element.position.y = y;
+                                }
+                                else {
+                                    element.position.x = x;
+                                    element.position.y = y;
+                                }
+                            }
+                            else {
+                                rVal = "No allready defined other block 2222 ";
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+        return rVal;
+    };
+
+    getAllData = () => {
+        return this.tData;
+    };
 }
 
 
+const cg = new ChessGame(taslar);
+
+(async () => {
+    setInterval(() => {
+        cg.run();
+    }, 100);
+})();
